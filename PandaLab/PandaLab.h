d6cc279277b2b19400fe5073c5fd309c0b4a91b4
Roger Alexander Arroyo Gonzales <aarroyo@cineplanet.com.pe>
2019-09-10 12:25:10 -0500

    Configuración inicial del proyecto

---
+++ PandaLab/PandaLab.h
@@ -0,0 +1,19 @@
+//
+//  PandaLab.h
+//  PandaLab
+//
+//  Created by aarroyo on 9/10/19.
+//  Copyright © 2019 PandaLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PandaLab.
+FOUNDATION_EXPORT double PandaLabVersionNumber;
+
+//! Project version string for PandaLab.
+FOUNDATION_EXPORT const unsigned char PandaLabVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PandaLab/PublicHeader.h>
+
+
---
+++ PandaLabExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PandaLabExamples
+//
+//  Created by aarroyo on 9/10/19.
+//  Copyright © 2019 PandaLab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
