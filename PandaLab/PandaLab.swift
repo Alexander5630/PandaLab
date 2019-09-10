d7dd481642c98e829e321e31a7ffccfec47c6ff6
Roger Alexander Arroyo Gonzales <aarroyo@cineplanet.com.pe>
2019-09-10 12:47:53 -0500

    Casos de prueba agregados

---
+++ PandaLab/PandaLab.swift
@@ -0,0 +1,21 @@
+//
+//  PandaLab.swift
+//  PandaLab
+//
+//  Created by aarroyo on 9/10/19.
+//  Copyright © 2019 PandaLab. All rights reserved.
+//
+
+public final class PandaLab {
+    
+    let name = "PandaLab"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
